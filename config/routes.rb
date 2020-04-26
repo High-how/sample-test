a6ad463fb2b7be237b8d9d99833779c45052836c
High-how <1215ikkou@gmail.com>
2020-04-26 16:43:26 +0900

    item-send

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "sample#show"
-  resources :sample, only: [:index, :new, :show]
+  root "sample#sample"
+  resources :sample, only: [:index, :new, :show, :sample]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
